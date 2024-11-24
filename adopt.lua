c5bc75fdbb746820567d9f3a2593fa88f5ffaa4e
greg66n <darien95sm@hotmail.com>
2024-11-23 21:12:20 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,4 +1,4 @@
-game:GetService("Players").LocalPlayer.UserId = math.random (1, 10^9)
+game:GetService("Players").LocalPlayer.UserId = a4b29ff
 wait(1)
 getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
 getgenv().custom_label = "VPS1"
