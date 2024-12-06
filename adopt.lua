e44b5744dd061edddf8ea66fe9b8805cdab88c2f
greg66n <darien95sm@hotmail.com>
2024-12-06 10:59:56 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,34 +1,6 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Spinzox/main/refs/heads/main/script.lua'),true))()
 wait(1)
--- Load bypass script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 
--- Repeat every 60 seconds
-while true do
-    wait(1)
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
-    local gingerbreadMarkersFolder = ReplicatedStorage:WaitForChild("Resources"):WaitForChild("IceSkating"):WaitForChild("GingerbreadMarkers")
-
-    -- Loop through each object in the GingerbreadMarkers folder
-    for _, marker in ipairs(gingerbreadMarkersFolder:GetChildren()) do
-        -- Create a new args table with the current marker's name
-        local args = {
-            [1] = marker.Name -- Use the name of the current marker
-        }
-
-        -- Invoke the server with the current marker's name
-        game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/PickUpGingerbread"):InvokeServer(unpack(args))
-
-        -- Wait for 0.1 seconds before the next call
-        wait(0.1)
-    end
-
-    wait(1)
-    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/RedeemPendingGingerbread"):FireServer()
-
-    -- Wait 60 seconds before repeating the process
-    wait(60)
-end
 
 wait(2)  -- Initial wait time
 
@@ -40,3 +12,5 @@ for i = 1, 25 do
     end
     wait(0.5)  -- Wait for 0.5 seconds before moving on to the next number
 end
+wait(1)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/greg66n/adopt/refs/heads/main/adopt1.lua'),true))()
