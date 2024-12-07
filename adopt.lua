c38b6ebf508d751fc7b7b1feaf2a80abdcdb7d29
greg66n <darien95sm@hotmail.com>
2024-12-07 07:58:27 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,2 +1,2 @@
-script_key="keyOkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
