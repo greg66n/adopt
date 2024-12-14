af2959a80aa64d0355a34ea6b1c8075ca8afe2e7
greg66n <darien95sm@hotmail.com>
2024-12-14 07:47:31 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,2 @@
-getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
-getgenv().custom_label = "VPS1"
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2G_WINTER_2024.lua"))()
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
