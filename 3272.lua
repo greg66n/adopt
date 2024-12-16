eb7cbc4be73b2397f69cd446f36f92eb31076646
greg66n <darien95sm@hotmail.com>
2024-12-15 20:25:24 -0800

    Update 3272.lua

--- 3272.lua
+++ 3272.lua
@@ -1,1 +1,14 @@
-1
+getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
+getgenv().custom_label = "VPS1"
+getgenv().BabyFarmActive = true
+getgenv().MiniGameFarm = true
+---------------------------------
+getgenv().PotFarm = true
+---------------------------------
+getgenv().PetFarm = false
+getgenv().PetTypes = { "grave owl", "kraken", "cheetah" }
+---------------------------------
+getgenv().EggFarm = false
+getgenv().EggTypes = { "garden egg", "royal egg" }
+---------------------------------
+loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
