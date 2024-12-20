25782c91d4ef78264fba9e67e2e77332a381b407
greg66n <darien95sm@hotmail.com>
2024-12-20 15:47:29 -0800

    Update 3272.lua

--- 3272.lua
+++ 3272.lua
@@ -1,2 +1,12 @@
+getgenv().Config = {
+    AutoFarm = {
+        FrostclawsRevenge = {
+            Enabled = true
+        },
+        Spleef = {
+            Enabled = false
+        }
+    }
+}
 script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
