c27d317ad25306e60cd40487dc2b5b4d63b39dd0
greg66n <darien95sm@hotmail.com>
2024-12-21 21:02:38 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,28 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-
--- Repeat every 60 seconds
-while true do
-    wait(1)
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
-    local gingerbreadMarkersFolder = ReplicatedStorage:WaitForChild("Resources"):WaitForChild("IceSkating"):WaitForChild("GingerbreadMarkers")
-
-    -- Loop through each object in the GingerbreadMarkers folder
-    for _, marker in ipairs(gingerbreadMarkersFolder:GetChildren()) do
-        -- Create a new args table with the current marker's name
-        local args = {
-            [1] = marker.Name -- Use the name of the current marker
+getgenv().Config = {
+    AutoFarm = {
+        FrostclawsRevenge = {
+            Enabled = true
+        },
+        Spleef = {
+            Enabled = false
         }
-
-        -- Invoke the server with the current marker's name
-        game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/PickUpGingerbread"):InvokeServer(unpack(args))
-
-        -- Wait for 0.1 seconds before the next call
-        wait(0.1)
-    end
-
-    wait(1)
-    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/RedeemPendingGingerbread"):FireServer()
-
-    -- Wait 60 seconds before repeating the process
-    wait(60)
-end
+    }
+}
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
