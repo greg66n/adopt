023fc31d7fbfb5cc09adbb6d758c21ecfba5b561
greg66n <darien95sm@hotmail.com>
2024-12-21 21:02:29 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,12 @@
-
+getgenv().Config = {
+    AutoFarm = {
+        FrostclawsRevenge = {
+            Enabled = true
+        },
+        Spleef = {
+            Enabled = false
+        }
+    }
+}
 script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d416a9fb566c94cba596550ef09d256b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
