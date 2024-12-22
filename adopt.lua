d61b20a32e285444fbfc9520c489fdfdb0221356
greg66n <darien95sm@hotmail.com>
2024-12-22 00:44:51 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,14 +1,12 @@
-getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
-getgenv().custom_label = "VPS1"
-getgenv().BabyFarmActive = false
-getgenv().MiniGameFarm = true
----------------------------------
-getgenv().PotFarm = false
----------------------------------
-getgenv().PetFarm = false
-getgenv().PetTypes = { "grave owl", "kraken", "cheetah" }
----------------------------------
-getgenv().EggFarm = false
-getgenv().EggTypes = { "garden egg", "royal egg" }
----------------------------------
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
+getgenv().Config = {
+    AutoFarm = {
+        FrostclawsRevenge = {
+            Enabled = true
+        },
+        Spleef = {
+            Enabled = false
+        }
+    }
+}
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
