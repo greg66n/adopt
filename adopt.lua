40aa0027a6538e9692c6cf956684c2d3522b51d5
greg66n <darien95sm@hotmail.com>
2024-12-24 03:22:20 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,6 +1,4 @@
-getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
-getgenv().custom_label = "VPS1"
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2G_FROSTCLAW_2024.lua"))()getgenv().Config = {
+getgenv().Config = {
     AutoFarm = {
         FrostclawsRevenge = {
             Enabled = true
