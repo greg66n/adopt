a476234258b447292278ef2f2276047a0c367c76
greg66n <darien95sm@hotmail.com>
2024-12-25 12:36:42 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,15 @@
+
 getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
 getgenv().custom_label = "VPS1"
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2G_FROSTCLAW_2024.lua"))()
+getgenv().BabyFarmActive = true
+getgenv().MiniGameFarm = true
+---------------------------------
+getgenv().PotFarm = true
+---------------------------------
+getgenv().PetFarm = false
+getgenv().PetTypes = { "grave owl", "kraken", "cheetah" }
+---------------------------------
+getgenv().EggFarm = false
+getgenv().EggTypes = { "garden egg", "royal egg" }
+---------------------------------
+loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
