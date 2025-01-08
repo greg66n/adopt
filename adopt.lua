7745fcfc36b2dfdb2e17b9785080dfb11c941e57
greg66n <darien95sm@hotmail.com>
2025-01-08 06:23:17 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,5 +1,14 @@
-getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
-getgenv().custom_label = "VPS1"
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2G_FROSTCLAW_2024.lua"))()
+getgenv().Config = {
+    AutoFarm = {
+        FrostclawsRevenge = {
+            Enabled = true
+        },
+        Spleef = {
+            Enabled = false
+        }
+    }
+}
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
 wait(5)
 game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/UseExchangeKiosk"):InvokeServer()
