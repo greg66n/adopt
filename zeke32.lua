51c785be74d861e1a584a275d0d5873e80da5f63
greg66n <darien95sm@hotmail.com>
2025-01-10 11:15:37 -0800

    Update zeke32.lua

--- zeke32.lua
+++ zeke32.lua
@@ -1,14 +1,44 @@
-getgenv().Config = {
-    AutoFarm = {
-        FrostclawsRevenge = {
-            Enabled = true
-        },
-        Spleef = {
-            Enabled = false
+Config = {
+    PetFarm = {
+        Enabled = true,            -- Master switch for pet farming
+        AutoNeon = false,          -- Makes neon pets when you have 4 full grown
+        FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
+        PrioritizeFriendship = true, -- true = highest friendship pets | false = by age only
+        FarmEggs = false,           -- Farm eggs until they hatch then buys more
+        EggType = "cracked_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
+        SelectFarmEgg = "any",    
+        BuyEggs = false,           -- Auto buys eggs when needed
+        AutoAgePotionEnabled = false,
+        AutoAgePotionPets = {"winter_2024_ice_cube","winter_2024_berry_cool_cube"}, -- example of how u use multiple pets
+        RarityFarming = {
+            Enabled = false, -- enable for rarity farming on wor
+            Order = {
+                "ultra_rare", -- highest priority 
+                "common",
+                "legendary",
+                "rare",
+                "uncommon" -- lowest priority 
+            }
         }
+    },
+    BabyFarm = {
+        Enabled = true,           -- Master switch for baby farming
+        Priority = "BabyFirst",    -- BabyFirst = do baby tasks first | PetFirst = do pet tasks first
+    },
+    Webhook = {
+        Enabled = false,            -- Enable Discord alerts
+        URL = "webhook here",          -- Your webhook
+    },
+    FPSSaver = {
+        Enabled = true,           -- FPS boost mode
+        Disable3DRendering = true, -- Max performance
+        FPSCap = 60                -- FPS cap 
+    },
+    UI = {
+        Maximized = false,          -- true = big | false = small
+        Scale = 1                  -- 0.1 = tiny, 0.5 = half, 1 = full
     }
 }
+
 script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/138e2f78b57c8e78354ac45978d60113.lua"))()
-wait(5)
-game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("WinterEventAPI/UseExchangeKiosk"):InvokeServer()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a2399ec22841391ee52540ec7c001e1.lua"))() --Adopt Me Potion Farm / Mass Farming
