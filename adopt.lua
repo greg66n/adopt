650a88956f96af9decca4ae538996f767aa694dc
greg66n <darien95sm@hotmail.com>
2025-01-11 23:51:42 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,20 +1,46 @@
-getgenv().hub_key = "4d964454191739793dcfa2a29ed7193a"
-getgenv().custom_label = "VPS1"
-getgenv().BabyFarmActive = true
-getgenv().MiniGameFarm = false
----------------------------------
-getgenv().PotFarm = true
----------------------------------
-getgenv().PetFarm = false
-getgenv().PetTypes = { "grave owl", "kraken", "cheetah" }
----------------------------------
-getgenv().EggFarm = false
-getgenv().EggTypes = { "garden egg", "royal egg" }
----------------------------------
-getgenv().AutoNeon = false
-getgenv().AutoMega = false
----------------------------------
-loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+Config = {
+    PetFarm = {
+        Enabled = true,            -- Master switch for pet farming
+        AutoNeon = false,          -- Makes neon pets when you have 4 full grown
+        FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
+        PrioritizeFriendship = true, -- true = highest friendship pets | false = by age only
+        FarmEggs = false,           -- Farm eggs until they hatch then buys more
+        EggType = "cracked_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
+        SelectFarmEgg = "any",    
+        BuyEggs = false,           -- Auto buys eggs when needed
+        AutoAgePotionEnabled = false,
+        AutoAgePotionPets = {"winter_2024_ice_cube","winter_2024_berry_cool_cube"}, -- example of how u use multiple pets
+        RarityFarming = {
+            Enabled = false, -- enable for rarity farming on wor
+            Order = {
+                "ultra_rare", -- highest priority 
+                "common",
+                "legendary",
+                "rare",
+                "uncommon" -- lowest priority 
+            }
+        }
+    },
+    BabyFarm = {
+        Enabled = true,           -- Master switch for baby farming
+        Priority = "PetFirst",    -- BabyFirst = do baby tasks first | PetFirst = do pet tasks first
+    },
+    Webhook = {
+        Enabled = false,            -- Enable Discord alerts
+        URL = "webhook here",          -- Your webhook
+    },
+    FPSSaver = {
+        Enabled = false,           -- FPS boost mode
+        Disable3DRendering = false, -- Max performance
+        FPSCap = 60                -- FPS cap 
+    },
+    UI = {
+        Maximized = false,          -- true = big | false = small
+        Scale = 1                  -- 0.1 = tiny, 0.5 = half, 1 = full
+    }
+}
 
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a2399ec22841391ee52540ec7c001e1.lua"))() --Adopt Me Potion Farm / Mass Farming
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
