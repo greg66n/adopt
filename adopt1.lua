c43736cb3026542719300c67a4ef9e72b75fe9f1
greg66n <darien95sm@hotmail.com>
2025-01-12 17:15:32 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,3 +1,12 @@
-getgenv().PetFarm = true
-_G.key = "Freekey999"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Hiraeth127/KeySystem/refs/heads/main/KeySystem.lua'))()
+-- ARCEUS, CODEX ETC
+script_key=NxzplPKcRYahKMvLcZtfTAOQURrRclwP;
+getgenv().Config = {
+    ["Winter_EVENT"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
