57cff51f284752f082ba2d35288c6d7dc1e67c18
greg66n <darien95sm@hotmail.com>
2025-01-13 06:21:15 -0800

    Update zeke32.lua

--- zeke32.lua
+++ zeke32.lua
@@ -10,3 +10,85 @@ getgenv().Config = {
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+wait(5)
+local args = {
+    [1] = "gifts",
+    [2] = "winter_2024_ice_tray",
+    [3] = {
+        ["buy_count"] = 25
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "pet_accessories",
+    [2] = "winter_2024_tree_skirt",
+    [3] = {
+        ["buy_count"] = 3
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "toys",
+    [2] = "winter_2024_festive_pogo",
+    [3] = {
+        ["buy_count"] = 1
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "pet_accessories",
+    [2] = "winter_2024_adopt_lanyard",
+    [3] = {
+        ["buy_count"] = 2
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "transport",
+    [2] = "winter_2024_chairlift",
+    [3] = {
+        ["buy_count"] = 2
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "toys",
+    [2] = "winter_2024_gift_sack_pogo",
+    [3] = {
+        ["buy_count"] = 1
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "pet_accessories",
+    [2] = "winter_2024_snowman_nose",
+    [3] = {
+        ["buy_count"] = 3
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
