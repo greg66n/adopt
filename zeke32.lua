29f5e716cb8d34336a2b42b17cee10910845c932
greg66n <darien95sm@hotmail.com>
2025-01-15 07:32:53 -0800

    Update zeke32.lua

--- zeke32.lua
+++ zeke32.lua
@@ -19,3 +19,78 @@ getgenv().Config = {
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
 end)
+wait(1)
+wait(1)
+local args = {
+    [1] = "pet_accessories",
+    [2] = "winter_2024_elf_shoes",
+    [3] = {
+        ["buy_count"] = 1
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "pets",
+    [2] = "winter_2024_frostclaw",
+    [3] = {
+        ["buy_count"] = 8
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+wait(1)
+
+local args = {
+    [1] = "strollers",
+    [2] = "winter_2024_snow_globe_stroller",
+    [3] = {
+        ["buy_count"] = 3
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+
+
+wait(1)
+
+local args = {
+    [1] = "gifts",
+    [2] = "winter_2024_sticker_pack",
+    [3] = {
+        ["buy_count"] = 150
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+
+wait(1)
+
+local args = {
+    [1] = "pets",
+    [2] = "winter_2024_great_pyrenees",
+    [3] = {
+        ["buy_count"] = 1
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+
+
+wait(1)
+
+local args = {
+    [1] = "pet_accessories",
+    [2] = "winter_2024_santas_bow",
+    [3] = {
+        ["buy_count"] = 3
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
