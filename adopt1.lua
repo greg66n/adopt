2a04a4f85962861338354bde6a8cc6320e107907
greg66n <darien95sm@hotmail.com>
2025-01-24 13:21:53 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,25 +1,3 @@
-
-
-task.spawn(function()
-    task.wait(35)
-    getgenv().API_KEY = "pQP1Ma1WE4h81mPToP54"
-    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
-end)
-
-task.spawn(function()
-   -- ARCEUS, CODEX ETC
-script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
-getgenv().Config = {
-    ["Winter_EVENT"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "942321951010086922",
-    ["Webhook"] = "https://discord.com/api/webhooks/123456",
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
-end)
 wait(1)
 local locations = {"Neighborhood", "MainMap"}
 
@@ -36,3 +14,26 @@ while true do  -- loop infinitely
     end
     wait(43200)  -- loop every 12h
 end
+
+task.spawn(function()
+    task.wait(35)
+    getgenv().API_KEY = "pQP1Ma1WE4h81mPToP54"
+    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
+end)
+wait(1)
+
+task.spawn(function()
+   -- ARCEUS, CODEX ETC
+script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
+getgenv().Config = {
+    ["Winter_EVENT"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "942321951010086922",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+end)
+
