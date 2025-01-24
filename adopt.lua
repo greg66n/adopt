4d6e6f92cf6599342ea854437c9f453e96783176
greg66n <darien95sm@hotmail.com>
2025-01-24 12:05:22 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -41,3 +41,19 @@ Config = {
 }
 script_key="script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d416a9fb566c94cba596550ef09d256b.lua"))()"
+wait(1)
+local locations = {"Neighborhood", "MainMap", "SkyCastle"}
+
+while true do  -- loop infinitely
+    for i = 1, 100 do
+        for _, location in ipairs(locations) do
+            local args = {
+                [1] = location,
+                [2] = tostring(i)
+            }
+            game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+            wait(0.5)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
+        end
+    end
+    wait(43200)  -- loop every 12h
+end
