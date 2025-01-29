601a94dd1fa9d72e9743020978cd9c49eaf399a3
greg66n <darien95sm@hotmail.com>
2025-01-28 18:05:13 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -25,14 +25,14 @@ wait(1)
 local locations = {"Neighborhood", "MainMap","SkyCastle"}
 
 while true do  -- loop infinitely
-    for i = 1, 100 do
+    for i = 1, 150 do
         for _, location in ipairs(locations) do
             local args = {
                 [1] = location,
                 [2] = tostring(i)
             }
             game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-            wait(0.5)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
+            wait(0.2)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
         end
     end
     wait(43200)  -- loop every 12h
