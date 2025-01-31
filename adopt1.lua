683abbfaa3107f5023fc03dffda7805cb2d00f12
greg66n <darien95sm@hotmail.com>
2025-01-31 01:35:59 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -22,16 +22,63 @@ end)
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(1)
-local locations = {"Neighborhood", "MainMap", "SkyCastle", "LNY2025GlitchZone"}
+local locations = {"MainMap", "LNY2025GlitchZone"}
 
-while true do  -- loop infinitely
+for _, location in ipairs(locations) do
     for i = 1, 100 do
-        for _, location in ipairs(locations) do
-            local args = {
-                [1] = location,
-                [2] = tostring(i)
-            }
-            game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-            wait(0.2)  -- 0.5 second delay prevent crash.. don't remove this to execute faster
-        end
+        local args = {
+            [1] = location,
+            [2] = tostring(i)
+        }
+        game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+        wait(0.2)  -- 0.1 second delay
     end
+end
+
+
+
+
+wait(1)
+local args = {
+    [1] = "pets",
+    [2] = "lunar_2025_blossom_snake",
+    [3] = {}
+}
+
+for i = 1, 8 do
+    game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+    wait(0.2) -- Delay of 0.2 seconds between each call
+end
+wait(1)
+
+local args = {
+    [1] = "pet_accessories",
+    [2] = "lunar_2025_knot_earrings", 
+    [3] = {}
+}
+
+game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "toys",
+    [2] = "lunar_2025_lucky_coin_flying_disc", 
+    [3] = {}
+}
+
+game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "pets",
+    [2] = "garden_2024_egg", 
+    [3] = {}
+}
+
+for i = 1, 100 do
+    game:GetService("ReplicatedStorage").API:FindFirstChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+    wait(0.1) -- Delay of 0.1 seconds between each call
+end
+wait(1)
+local args = {
+    [1] = "toys",
+    [2] = "lunar_2025_lucky_coin_flying_disc", 
+    [3] = {}
