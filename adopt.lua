63ba29c678c1311792a19bd769a27d005f2a9034
greg66n <darien95sm@hotmail.com>
2025-02-01 19:15:29 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -22,7 +22,20 @@ end)
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(1)
-local locations = {"MainMap", "LNY2025GlitchZone", "MoonInterior"}
+local locations = {"MainMap"}
+
+for _, location in ipairs(locations) do
+    for i = 1, 100 do
+        local args = {
+            [1] = location,
+            [2] = tostring(i)
+        }
+        game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+        wait(0.2)  -- 0.1 second delay
+    end
+end
+wait(1)
+local locations = {"MoonInterior"}
 
 for _, location in ipairs(locations) do
     for i = 1, 100 do
