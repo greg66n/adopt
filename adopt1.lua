fb46ceab0018f905feac3fe78cf4b8519eabcfbd
greg66n <darien95sm@hotmail.com>
2025-02-02 18:33:05 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,6 +1,6 @@
 task.spawn(function()
     task.wait(35)
-    getgenv().API_KEY = "pQP1Ma1WE4h81mPToP54"
+    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
     loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
 end)
 
@@ -8,12 +8,12 @@ task.spawn(function()
    -- ARCEUS, CODEX ETC
 script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
 getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["Winter_EVENT"] = false,
     ["PetFarmActive"] = true,
     ["EggFarmActive"] = false,
     ["Blur_username"] = true,
     ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
+    ["DiscordId"] = "942321951010086922",
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
