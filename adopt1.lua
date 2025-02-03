277fed845cc5dde065e266e1cc52e92052db8186
greg66n <darien95sm@hotmail.com>
2025-02-02 17:46:08 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,21 +1,22 @@
 task.spawn(function()
     task.wait(35)
-    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
+    getgenv().API_KEY = "pQP1Ma1WE4h81mPToP54"
     loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
 end)
 
 task.spawn(function()
-    script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
+   -- ARCEUS, CODEX ETC
+script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
 getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["Winter_EVENT"] = false,
     ["PetFarmActive"] = true,
     ["EggFarmActive"] = false,
-    ["Blur_username"] = false,
+    ["Blur_username"] = true,
     ["Blazing_Lion_Log"] = false,
     ["DiscordId"] = "942321951010086922",
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()-- add other scripts here
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
 end)
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
