1d6a1673e53fc1c97b3dc0c7ae32e3a099215d35
greg66n <darien95sm@hotmail.com>
2025-02-02 17:50:13 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -8,12 +8,12 @@ task.spawn(function()
    -- ARCEUS, CODEX ETC
 script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
 getgenv().Config = {
-    ["Winter_EVENT"] = false,
+    ["PetFarmAutoSwitchFullGrown"] = false,
     ["PetFarmActive"] = true,
     ["EggFarmActive"] = false,
     ["Blur_username"] = true,
     ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "942321951010086922",
+    ["DiscordId"] = "123456",
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
