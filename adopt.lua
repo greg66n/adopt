1fd5f70b7f0ef868d82184e8346276aba9c26367
greg66n <darien95sm@hotmail.com>
2025-02-03 05:03:57 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -21,14 +21,31 @@ end)
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(1)
-local args = {
-    [1] = "MoonInterior",
-    [3] = true
-}
+local locations = {"MainMap", "MoonInterior"}
+local args = {}
 
-for i = 1, 100 do
-    args[2] = tostring(i)
-    game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-    wait(0.2)  -- Delay of 0.2 seconds
+-- Function to collect stars
+local function collectStars(location, isSpecial)
+    for i = 1, 100 do
+        args[1] = location
+        args[2] = tostring(i)
+        
+        -- If it's a special star, add the third argument
+        if isSpecial then
+            args[3] = true
+        else
+            args[3] = nil
+        end
+        
+        game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
+        wait(0.2)  -- Delay of 0.2 seconds
+    end
 end
 
+-- Collect stars for both locations
+for _, location in ipairs(locations) do
+    collectStars(location, false)  -- Regular stars
+    wait(1)  -- Wait 1 second before moving to the next location
+    collectStars(location, true)   -- Special stars
+    wait(1)  -- Wait 1 second before moving to the next location
+end
