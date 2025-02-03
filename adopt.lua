6121379479eb40780198425a78dc4c2d9e73515a
greg66n <darien95sm@hotmail.com>
2025-02-03 04:27:24 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,52 +1,44 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(5)
-local locations = {"MainMap"}
-
-for _, location in ipairs(locations) do
-    for i = 1, 100 do
-        local args = {
-            [1] = location,
-            [2] = tostring(i)
+Config = {
+    PetFarm = {
+        Enabled = true,            -- Master switch for pet farming
+        AutoNeon = true,          -- Makes neon pets when you have 4 full grown
+        FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
+        PrioritizeFriendship = true, -- true = highest friendship pets | false = by age only
+        FarmEggs = false,           -- Farm eggs until they hatch then buys more
+        EggType = "cracked_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
+        SelectFarmEgg = "any",    
+        BuyEggs = false,           -- Auto buys eggs when needed
+        AutoAgePotionEnabled = false,
+        AutoAgePotionPets = {"winter_2024_ice_cube","winter_2024_berry_cool_cube"}, -- example of how u use multiple pets
+        RarityFarming = {
+            Enabled = false, -- enable for rarity farming on wor
+            Order = {
+                "ultra_rare", -- highest priority 
+                "common",
+                "legendary",
+                "rare",
+                "uncommon" -- lowest priority 
+            }
         }
-        game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-        wait(0.2)  -- 0.1 second delay
-    end
-end
-wait(1)
-local locations = {"MoonInterior"}
-
-for _, location in ipairs(locations) do
-    for i = 1, 100 do
-        local args = {
-            [1] = location,
-            [2] = tostring(i)
-        }
-        game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-        wait(0.2)  -- 0.1 second delay
-    end
-end
-wait(1)
-local args = {
-    [1] = "MoonInterior",
-    [3] = true
+    },
+    BabyFarm = {
+        Enabled = true,           -- Master switch for baby farming
+        Priority = "BabyFirst",    -- BabyFirst = do baby tasks first | PetFirst = do pet tasks first
+    },
+    Webhook = {
+        Enabled = true,            -- Enable Discord alerts
+        URL = "webhook here",          -- Your webhook
+    },
+    FPSSaver = {
+        Enabled = false,           -- FPS boost mode
+        Disable3DRendering = false, -- Max performance
+        FPSCap = 60                -- FPS cap 
+    },
+    UI = {
+        Maximized = false,          -- true = big | false = small
+        Scale = 1                  -- 0.1 = tiny, 0.5 = half, 1 = full
+    }
 }
 
-for i = 1, 100 do
-    args[2] = tostring(i)
-    game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-    wait(0.2)  -- Delay of 0.2 seconds
-end
-end
-wait(1)
-local args = {
-    [1] = "MainMap",
-    [3] = true
-}
-
-for i = 1, 100 do
-    args[2] = tostring(i)
-    game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ShootingStarCollected"):FireServer(unpack(args))
-    wait(0.2)  -- Delay of 0.2 seconds
-end
-
-
+script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a2399ec22841391ee52540ec7c001e1.lua"))() --Adopt Me Potion Farm / Mass Farming
