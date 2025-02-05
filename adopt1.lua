0fe74e65f02f0babb8a1f5b0deb415b1e551e877
greg66n <darien95sm@hotmail.com>
2025-02-04 16:01:33 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -32,6 +32,12 @@ for _, location in ipairs(locations) do
 end
 
 wait(1)
+local args = {
+    [1] = "MoonInterior"
+}
+
+game:GetService("ReplicatedStorage").API:FindFirstChild("MoonAPI/ClaimRoyalEgg"):FireServer(unpack(args))
+wait(1)
 task.spawn(function()
     task.wait(35)
     getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
