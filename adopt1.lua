21cf783e0cebf68b67b9a7746135423a24de3764
greg66n <darien95sm@hotmail.com>
2025-02-08 09:11:18 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -60,7 +60,7 @@ Config = {
         Enabled = true,            -- Master switch for pet farming
         AutoNeon = false,          -- Makes neon pets when you have 4 full grown
         FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
-        PrioritizeFriendship = true, -- true = highest friendship pets | false = by age only
+        PrioritizeFriendship = false, -- true = highest friendship pets | false = by age only
         FarmEggs = true,           -- Farm eggs until they hatch then buys more
         EggType = "moon_2025_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
         SelectFarmEgg = "any",
