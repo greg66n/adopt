1dc72c9266e40f45911a16fb9db1614b3a7dba94
greg66n <darien95sm@hotmail.com>
2025-02-09 23:12:09 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,4 +1,4 @@
-wait(15)
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(1)
 local locations = {"MainMap", "MoonInterior"}
