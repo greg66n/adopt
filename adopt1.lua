ab90c0a65e6898897465b13513fa09055e0d86c9
greg66n <darien95sm@hotmail.com>
2025-02-22 15:28:52 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -116,52 +116,6 @@ for i = 1, 8 do
     game:GetService("ReplicatedStorage").API:FindFirstChild("ValentinesEventAPI/PickupRoseHearts"):FireServer(unpack(secondHeartArgs))
     wait(0.1)  -- Wait for 1 second
 end
-
-wait(0.1)
-Config = {
-    PetFarm = {
-        Enabled = true,            -- Master switch for pet farming
-        AutoNeon = false,          -- Makes neon pets when you have 4 full grown
-        FarmUntilFullyGrown = false, -- true = farms youngest pets | false = oldest pets
-        PrioritizeFriendship = false, -- true = highest friendship pets | false = by age only
-        FarmEggs = true,           -- Farm eggs until they hatch then buys more
-        EggType = "moon_2025_egg",   -- cracked_egg, regular_pet_egg, royal_egg, garden_2024_egg
-        SelectFarmEgg = "moon_2025_egg",
-        BuyEggs = true,           -- Auto buys eggs when needed
-        AutoAgePotionEnabled = false,
-        AutoAgePotionPets = {"winter_2024_ice_cube","winter_2024_berry_cool_cube"}, -- example of how u use multiple pets
-        RarityFarming = {
-            Enabled = false, -- enable for rarity farming on wor
-            Order = {
-                "ultra_rare", -- highest priority 
-                "common",
-                "legendary",
-                "rare",
-                "uncommon" -- lowest priority 
-            }
-        }
-    },
-    BabyFarm = {
-        Enabled = true,           -- Master switch for baby farming
-        Priority = "BabyFirst",    -- BabyFirst = do baby tasks first | PetFirst = do pet tasks first
-    },
-    Webhook = {
-        Enabled = true,            -- Enable Discord alerts
-        URL = "webhook here",          -- Your webhook
-    },
-    FPSSaver = {
-        Enabled = false,           -- FPS boost mode
-        Disable3DRendering = false, -- Max performance
-        FPSCap = 10                -- FPS cap 
-    },
-    UI = {
-        Maximized = false,          -- true = big | false = small
-        Scale = 1                  -- 0.1 = tiny, 0.5 = half, 1 = full
-    }
-}
-
-script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a2399ec22841391ee52540ec7c001e1.lua"))() --Adopt Me Potion Farm / Mass Farming 
 wait(0.1)
 local args = {
     [1] = "pets",
