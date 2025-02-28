e768d5bf93e0444c56a8ee18fe347ef159b31430
greg66n <darien95sm@hotmail.com>
2025-02-28 05:33:17 -0800

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -123,9 +123,9 @@ for i = 1, 8 do
     wait(0.1)  -- Wait for 1 second
 end
 wait(0.1)
- local args = {
+  local args = {
         [1] = "toys",
-        [2] = "valentines_2025_heart_throwing_disc",
+        [2] = "valentines_2025_cupids_bow_grappling_hook",
         [3] = {}
     }
     
