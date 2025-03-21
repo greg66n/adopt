4799cd3138b21438feeeda162796bd124c487587
greg66n <darien95sm@hotmail.com>
2025-03-21 14:21:24 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,34 +1,6 @@
-
-script_key="SxUwNCauqKtHFATeixFiAjdgLHnBJOHW";
 getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+    Enabled = true,
+    NotPublicServer = false -- if your gonna use multiple accounts in one private server or just a private server use this
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
-
-wait(10)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(5)
-local args = {
-    [1] = "claim_jacket"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-wait()
-local args = {
-    [1] = "claim_shoes"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-wait(1)
-local args = {
-    [1] = "claim_hat"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-waut(1)
+script_key =OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH""
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/55d3027b8606691d3ca80082c43c0cb8.lua"))()
