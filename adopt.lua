510012c0c16a5f36646bf4290213bd76a30e0f17
greg66n <darien95sm@hotmail.com>
2025-03-21 04:36:16 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,21 +1,24 @@
+wait(10)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(5)
 local args = {
     [1] = "claim_jacket"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-
+wait()
 local args = {
     [1] = "claim_shoes"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-
+wait(1)
 local args = {
     [1] = "claim_hat"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
-
+waut(1)
 script_key="SxUwNCauqKtHFATeixFiAjdgLHnBJOHW";
 getgenv().Config = {
     ["PetFarmAutoSwitchFullGrown"] = false,
