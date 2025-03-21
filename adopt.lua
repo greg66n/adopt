7c88e48b0f0df94f3138375f71c85604b289ae9b
greg66n <darien95sm@hotmail.com>
2025-03-21 04:55:28 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,16 @@
+
+script_key="SxUwNCauqKtHFATeixFiAjdgLHnBJOHW";
+getgenv().Config = {
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+
 wait(10)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
 wait(5)
@@ -19,14 +32,3 @@ local args = {
 
 game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("StPatricksAPI/AttemptClaim"):InvokeServer(unpack(args))
 waut(1)
-script_key="SxUwNCauqKtHFATeixFiAjdgLHnBJOHW";
-getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "https://discord.com/api/webhooks/123456",
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
