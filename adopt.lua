b1c562708d93902c21d921cbef92f52c6a64e3ac
greg66n <darien95sm@hotmail.com>
2025-03-22 14:53:05 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -2,5 +2,5 @@ getgenv().Config = {
     Enabled = true,
     NotPublicServer = false -- if your gonna use multiple accounts in one private server or just a private server use this
 }
-script_key =OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH""
+script_key ="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH"
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/55d3027b8606691d3ca80082c43c0cb8.lua"))()
