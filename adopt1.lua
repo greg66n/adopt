c4d678791fc41708d4749f87d257045032b9f749
greg66n <darien95sm@hotmail.com>
2025-05-19 23:32:33 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,12 +1,39 @@
-script_key="oEIseaEyLcDiaxDqeDAIUxhGCAPuLBOM";
-getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["HideUselessGui"] = true,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "",
+Config = {
+    PetFarm = {
+        Enabled = true,
+        AutoNeon = false,
+        FarmUntilFullyGrown = false,
+        PrioritizeFriendship = true,
+        FarmEggs = false,
+        EggType = "cracked_egg",
+        SelectFarmEgg = "any",
+        BuyEggs = false,
+        AutoAgePotionEnabled = false,
+        AutoAgePotionPets = { "winter_2024_ice_cube", "winter_2024_berry_cool_cube" },
+        ExcludedTasks = {
+            Enabled = false,
+            Tasks = {
+                "camping", "hungry", "thirsty", "sick", "sleepy", "dirty", "bored", "toilet",
+                "school", "salon", "beach_party", "pizza_party", "play", "walk", "ride", "moon",
+                "pet_me", "catch"
+            }
+        },
+        RarityFarming = {
+            Enabled = false,
+            Order = { "ultra_rare", "common", "legendary", "rare", "uncommon" }
+        }
+    },
+    BabyFarm = {
+        Enabled = true,
+        Priority = "PetFirst",
+        ExcludedTasks = {
+            Enabled = false,
+            Tasks = { "camping" }
+        }
+    },
+    Webhook = {
+        Enabled = true,
+        URL = ""
+    }
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeMassFarm.lua"))()
