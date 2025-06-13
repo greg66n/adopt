d38299e54331269adf942009560941ffda5a5f98
greg66n <darien95sm@hotmail.com>
2025-06-13 12:50:40 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,28 +1,39 @@
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(1)
-for i = 1, 99 do
-    local args = {
-        "food",
-        "butterfly_2025_snapdragon_flower",
-        1
+Config = {
+    PetFarm = {
+        Enabled = true,
+        AutoNeon = true,
+        FarmUntilFullyGrown = false,
+        PrioritizeFriendship = true,
+        FarmEggs = false,
+        EggType = "cracked_egg",
+        SelectFarmEgg = "any",
+        BuyEggs = false,
+        AutoAgePotionEnabled = false,
+        AutoAgePotionPets = { "winter_2024_ice_cube", "winter_2024_berry_cool_cube" },
+        ExcludedTasks = {
+            Enabled = false,
+            Tasks = {
+                "camping", "hungry", "thirsty", "sick", "sleepy", "dirty", "bored", "toilet",
+                "school", "salon", "beach_party", "pizza_party", "play", "walk", "ride", "moon",
+                "pet_me", "catch"
+            }
+        },
+        RarityFarming = {
+            Enabled = false,
+            Order = { "ultra_rare", "common", "legendary", "rare", "uncommon" }
+        }
+    },
+    BabyFarm = {
+        Enabled = true,
+        Priority = "PetFirst",
+        ExcludedTasks = {
+            Enabled = false,
+            Tasks = { "camping" }
+        }
+    },
+    Webhook = {
+        Enabled = true,
+        URL = ""
     }
-    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
-    task.wait(0.1)
-end
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diwserenityhub/other/refs/heads/main/butterfly_auto_open.lua"))()
-wait(1)
-
-script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
-getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["HideUselessGui"] = true,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "",
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeMassFarm.lua"))()
