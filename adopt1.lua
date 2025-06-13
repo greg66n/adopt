ae3c62be56a4e610c7fa3c91a384bf7fde4a97b2
greg66n <darien95sm@hotmail.com>
2025-06-12 23:27:08 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,3 +1,32 @@
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(1)
+for i = 1, 99 do
+    local args = {
+        "food",
+        "butterfly_2025_snapdragon_flower",
+        1
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
+    task.wait(0.1)
+end
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/diwserenityhub/other/refs/heads/main/butterfly_auto_open.lua"))()
+wait(1)
+
+script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
+getgenv().Config = {
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["HideUselessGui"] = true,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+
 script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
 getgenv().Config = {
     ["PetFarmAutoSwitchFullGrown"] = false,
