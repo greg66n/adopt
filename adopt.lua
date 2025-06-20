af9ec942fd34bb2cf879018a6ff16f5ee2b8ea1e
greg66n <darien95sm@hotmail.com>
2025-06-19 22:51:50 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,2 +1,16 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+
+
+script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
+getgenv().Config = {
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["HideUselessGui"] = true,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
