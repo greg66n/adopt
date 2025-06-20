17ff4df5c616a7ea7d36a531847f6464d4354828
greg66n <darien95sm@hotmail.com>
2025-06-20 05:13:26 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -15,3 +15,15 @@ for i = 1, 999 do
 
     task.wait(0.1) -- Additional wait to maintain 0.2s total loop interval
 end
+script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
+getgenv().Config = {
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["HideUselessGui"] = true,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
