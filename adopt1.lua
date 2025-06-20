845b505a98e4a498d517daa42e09344d31f7801c
greg66n <darien95sm@hotmail.com>
2025-06-20 01:42:47 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -61,8 +61,6 @@ for i = 1, 35 do
     game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
     task.wait(0.2)
 end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diwserenityhub/other/refs/heads/main/butterfly_auto_open.lua"))()
-
 
 
 
