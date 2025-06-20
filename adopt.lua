4d21e0bb84ec690554c3501f8066bd9c762d9d60
greg66n <darien95sm@hotmail.com>
2025-06-19 19:44:49 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,13 +1,1 @@
 
-script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
-getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["HideUselessGui"] = true,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "",
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
