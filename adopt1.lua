37a4e4103a15b3ee1805a8e7c6061065ecc66224
greg66n <darien95sm@hotmail.com>
2025-06-19 20:15:49 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,13 +1,9 @@
-
-script_key="wuPUyNrPKcRDssQugQwsbVEsUbeVlCrC";
-getgenv().Config = {
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    ["HideUselessGui"] = true,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "",
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(0.1)
+while true do
+    local args = {
+        "food",
+        "butterfly_2025_snapdragon_flower",
+        1
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
