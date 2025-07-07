8b79ece37446e74622aa76cbbb2456e90ce24a64
greg66n <darien95sm@hotmail.com>
2025-07-07 02:14:13 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,4 +1,18 @@
-script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeEventFarm.lua"))()
+ocal plr = game:GetService("Players").LocalPlayer
+
+if plr.Character == nil then
+    repeat 
+        local playbutton = plr:WaitForChild("PlayerGui"):WaitForChild("NewsApp")
+            :WaitForChild("EnclosingFrame"):WaitForChild("MainFrame")
+            :WaitForChild("Buttons"):WaitForChild("PlayButton")
+        
+        firesignal(playbutton.MouseButton1Down)
+        firesignal(playbutton.MouseButton1Up)
+        firesignal(playbutton.MouseButton1Click)
+        
+        task.wait(2)
+    until plr.Character ~= nil
+end
 wait(5)
 setthreadidentity(2)
 local load = require(game:GetService("ReplicatedStorage").Fsys).load
@@ -8,3 +22,5 @@ local function enterinterior(name)
 task.spawn(enter, name, "", {})
 end
 enterinterior("MainMap!Snow")
+wait(5)
+script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeEventFarm.lua"))()
