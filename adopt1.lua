993283387284968e13065b685779f9806f93e1ab
greg66n <darien95sm@hotmail.com>
2025-07-07 01:58:44 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,1 +1,1 @@
-script_key="OkbUgSyCLieCMbeVqgTZOAgbKIdxjRLH" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeEventFarm.lua"))()
+script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeEventFarm.lua"))()
