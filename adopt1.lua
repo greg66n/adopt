421b13eca7510b0a7e75c9324f5fafcf1a2cc7a5
greg66n <darien95sm@hotmail.com>
2025-07-07 02:03:27 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,1 +1,10 @@
 script_key="YDizWtCsoUWCWDMoEGyWgIZZjPCgquVY" loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMeEventFarm.lua"))()
+wait(5)
+setthreadidentity(2)
+local load = require(game:GetService("ReplicatedStorage").Fsys).load
+local interiors = load("InteriorsM")
+local enter = interiors.enter
+local function enterinterior(name)
+task.spawn(enter, name, "", {})
+end
+enterinterior("MainMap!Snow")
