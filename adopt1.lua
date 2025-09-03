158fd7c6060a53379b59e11b89841936ca08f811
greg66n <darien95sm@hotmail.com>
2025-09-03 16:11:23 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -40,29 +40,7 @@ getgenv().farmsettings.gui = getgenv().farmsettings.gui == nil and true or getge
 repeat task.wait() until game:IsLoaded()
 task.wait(2.5)
 setthreadidentity(2)
--- Services and modules
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
-wait(0.1)
--- Define the core function to claim the reward
-local function claimReward(tier)
-    local args = {
-        "house_pets_2025_pass_1",
-        tier
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("BattlePassAPI/ClaimReward"):InvokeServer(unpack(args))
-end
 
--- Main loop to run the script
-while true do
-    -- Loop from 1 to 20 for the reward tiers
-    for i = 1, 20 do
-        claimReward(i)
-        -- Wait 0.5 seconds before the next claim
-        task.wait(0.5)
-    end
-    -- Wait 30 seconds before starting the cycle again
-    task.wait(30)
-end
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local HttpService = game:GetService("HttpService")
