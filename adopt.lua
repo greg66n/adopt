dc92a7f1337384fdb76328af202a64ba1abcbcdc
greg66n <darien95sm@hotmail.com>
2025-09-05 16:47:49 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -4,7 +4,7 @@ wait(0.1)
 -- Define the core function to claim the reward
 local function claimReward(tier)
     local args = {
-        "house_pets_2025_pass_1",
+        "house_pets_2025_pass_2",
         tier
     }
     game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("BattlePassAPI/ClaimReward"):InvokeServer(unpack(args))
@@ -13,7 +13,7 @@ end
 -- Function to reset the battle pass
 local function resetPass()
     local args = {
-        "house_pets_2025_pass_1"
+        "house_pets_2025_pass_2"
     }
     game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("BattlePassAPI/AttemptBattlePassReset"):InvokeServer(unpack(args))
 end
