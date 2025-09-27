183f7de7c639a58554c09b97cf3f447bdcc5b214
greg66n <darien95sm@hotmail.com>
2025-09-27 08:15:33 -0700

    Update adopt.lua

--- adopt.lua
+++ adopt.lua
@@ -1,1 +1,18 @@
 
+script_key="WorkFine";
+getgenv().Config = {
+    ["PetAMountToBuy"] = "0", -- can be 999
+    ["PetRemoteToBuy"] = "moon_2025_egg",
+
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = false,
+    ["LitePetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    
+    ["HideUselessGui"] = true,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
