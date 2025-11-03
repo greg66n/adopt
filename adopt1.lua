774fc90db3102485c3e0bfcde9f84406fa6c8890
greg66n <darien95sm@hotmail.com>
2025-11-02 22:35:17 -0800

    Clean up adopt1.lua by removing unused code
    Removed commented-out configuration settings and redundant script key.

--- adopt1.lua
+++ adopt1.lua
@@ -1,3 +1,73 @@
+
+script_key = "VuEAzrzDNFjpnnPXCwaBNyjeRkwCfdnm"
+
+--[[
+getgenv().BUY_BEFORE_FARMING = {
+    {NameId = "aztec_egg_2025_aztec_egg", MaxAmount = 20},
+}
+--]]
+
+--[[
+getgenv().OPEN_ITEMS_BEFORE_FARMING = {
+    "butterfly_2025_snapdragon_flower"
+}
+--]]
+
+--[[
+getgenv().AGE_PETS_BEFORE_FARMING = {
+    "st_patricks_2025_clover_cow",
+}
+--]]
+
+
+getgenv().SETTINGS = {
+    ENABLE_AUTO_FARM = true,
+    SET_FPS = 2,
+
+    FOCUS_FARM_AGE_POTION = true, 
+
+    PETS_TO_AGE_IN_PEN = {
+        "pet_recycler_2025_basic_egg",
+        "aztec_egg_2025_tegu",
+        "aztec_egg_2025_jungle_frog",
+        "aztec_egg_2025_ahuizotl",
+        "aztec_egg_2025_xiucohtl",
+        "aztec_egg_2025_ehecatl"
+    },
+    
+    ENABLE_RELEASE_PETS = false, -- if true it will use the pets in PETS_TO_AGE_IN_PEN to RELEASE
+
+    PET_AUTO_FUSION = false,
+    DEBUG_MODE = false,
+    POTATO_MODE = true,
+
+    HATCH_EGG_PRIORITY = false,
+    HATCH_EGG_PRIORITY_NAMES = {
+        "aztec_egg_2025_aztec_egg", 
+    },
+
+    PET_ONLY_PRIORITY = false,
+    PET_ONLY_PRIORITY_NAMES = {
+        "moon_2025_glormy_dolphin",
+    },
+
+    ENABLE_TRADE_COLLECTOR = true,
+    TRADE_ONLY_NEON_LUMINOUS_AND_MEGA = true,
+    TRADE_COLLECTOR_NAME = {
+        "input_Username_Here1",
+        "input_Username_Here2_ect",
+    },
+
+    TRADE_LIST = {
+        "pet_recycler_2025_giant_panda",
+        "ice_dimension_2025_frostbite_bear",
+    },
+
+    ENABLE_TRASH_COLLECTOR = false, -- it has its own list of trash pets
+    TRASH_COLLECTOR_NAMES = {"input_Username_Here_for_TRASH"},
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7359a8866fd351cc305781fb0b0ecfc.lua"))()
 wait(10)
 -- Robust purchase runner with stickers first
 --// Load Bypass
@@ -509,72 +579,3 @@ end
 print("===== ALL DONE =====")
 
 wait(1)
-script_key = "VuEAzrzDNFjpnnPXCwaBNyjeRkwCfdnm"
-
---[[
-getgenv().BUY_BEFORE_FARMING = {
-    {NameId = "aztec_egg_2025_aztec_egg", MaxAmount = 20},
-}
---]]
-
---[[
-getgenv().OPEN_ITEMS_BEFORE_FARMING = {
-    "butterfly_2025_snapdragon_flower"
-}
---]]
-
---[[
-getgenv().AGE_PETS_BEFORE_FARMING = {
-    "st_patricks_2025_clover_cow",
-}
---]]
-
-
-getgenv().SETTINGS = {
-    ENABLE_AUTO_FARM = true,
-    SET_FPS = 2,
-
-    FOCUS_FARM_AGE_POTION = true, 
-
-    PETS_TO_AGE_IN_PEN = {
-        "pet_recycler_2025_basic_egg",
-        "aztec_egg_2025_tegu",
-        "aztec_egg_2025_jungle_frog",
-        "aztec_egg_2025_ahuizotl",
-        "aztec_egg_2025_xiucohtl",
-        "aztec_egg_2025_ehecatl"
-    },
-    
-    ENABLE_RELEASE_PETS = false, -- if true it will use the pets in PETS_TO_AGE_IN_PEN to RELEASE
-
-    PET_AUTO_FUSION = false,
-    DEBUG_MODE = false,
-    POTATO_MODE = true,
-
-    HATCH_EGG_PRIORITY = false,
-    HATCH_EGG_PRIORITY_NAMES = {
-        "aztec_egg_2025_aztec_egg", 
-    },
-
-    PET_ONLY_PRIORITY = false,
-    PET_ONLY_PRIORITY_NAMES = {
-        "moon_2025_glormy_dolphin",
-    },
-
-    ENABLE_TRADE_COLLECTOR = true,
-    TRADE_ONLY_NEON_LUMINOUS_AND_MEGA = true,
-    TRADE_COLLECTOR_NAME = {
-        "input_Username_Here1",
-        "input_Username_Here2_ect",
-    },
-
-    TRADE_LIST = {
-        "pet_recycler_2025_giant_panda",
-        "ice_dimension_2025_frostbite_bear",
-    },
-
-    ENABLE_TRASH_COLLECTOR = false, -- it has its own list of trash pets
-    TRASH_COLLECTOR_NAMES = {"input_Username_Here_for_TRASH"},
-}
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7359a8866fd351cc305781fb0b0ecfc.lua"))()
