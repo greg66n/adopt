5b5bc0275f1bd3680df961c30f9033f55fb34db5
greg66n <darien95sm@hotmail.com>
2026-04-12 00:42:47 -0700

    Update adopt1.lua

--- adopt1.lua
+++ adopt1.lua
@@ -1,39 +1,130 @@
-task.spawn(function()
-    script_key="loQYbkohnAXqroVgjJDQsUraFKhyijVZ";
-    getgenv().Config = {
-        ["PetAMountToBuy"] = "0", -- can be 999
-        ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
-        ["PetRemoteToBuy"] = "", -- exemple for gifts halloween_2025_spider_box
+script_key = "VuEAzrzDNFjpnnPXCwaBNyjeRkwCfdnm"
+
+--[[
+getgenv().BUY_BEFORE_FARMING = {
+    {NameId = "aztec_egg_2025_aztec_egg", MaxAmount = 20},
+}
+--]]
+
+--[[
+getgenv().OPEN_ITEMS_BEFORE_FARMING = {
+    "butterfly_2025_snapdragon_flower"
+}
+--]]
+
+--[[
+getgenv().AGE_PETS_BEFORE_FARMING = {
+    "st_patricks_2025_clover_cow",
+}
+--]]
 
 
-        ["PetFarmAutoSwitchFullGrown"] = false,
-        ["PetFarmActive"] = true, -- farm potion
+getgenv().SETTINGS = {
+    ENABLE_AUTO_FARM = true,
+    SET_FPS = 2,
+
+    FOCUS_FARM_AGE_POTION = true, 
+
+    PETS_TO_AGE_IN_PEN = {
+        "pet_recycler_2025_basic_egg",
+        "aztec_egg_2025_tegu",
+        "aztec_egg_2025_jungle_frog",
+        "aztec_egg_2025_ahuizotl",
+        "aztec_egg_2025_xiucohtl",
+        "aztec_egg_2025_ehecatl"
+    },
+
+    ENABLE_RELEASE_PETS = false, -- if true it will use the pets in PETS_TO_AGE_IN_PEN to RELEASE
+
+    PET_AUTO_FUSION = false,
+    DEBUG_MODE = false,
+    POTATO_MODE = true,
+
+    HATCH_EGG_PRIORITY = false,
+    HATCH_EGG_PRIORITY_NAMES = {
+        "aztec_egg_2025_aztec_egg", 
+    },
+
+    PET_ONLY_PRIORITY = false,
+    PET_ONLY_PRIORITY_NAMES = {
+        "moon_2025_glormy_dolphin",
+    },
+
+    ENABLE_TRADE_COLLECTOR = true,
+    TRADE_ONLY_NEON_LUMINOUS_AND_MEGA = true,
+    TRADE_COLLECTOR_NAME = {
+        "input_Username_Here1",
+        "input_Username_Here2_ect",
+    },
+
+    TRADE_LIST = {
+        "pet_recycler_2025_giant_panda",
+        "ice_dimension_2025_frostbite_bear",
+    },
+
+    ENABLE_TRASH_COLLECTOR = false, -- it has its own list of trash pets
+    TRASH_COLLECTOR_NAMES = {"input_Username_Here_for_TRASH"},
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7359a8866fd351cc305781fb0b0ecfc.lua"))()script_key = "VuEAzrzDNFjpnnPXCwaBNyjeRkwCfdnm"
+
+getgenv().BUY_BEFORE_FARMING = {
+    {NameId = "cracked_egg", MaxAmount = 0},
+}
+--]]
+--[[
+getgenv().OPEN_ITEMS_BEFORE_FARMING = {
+    "butterfly_2025_snapdragon_flower"
+}
+--]]
+--[[
+getgenv().AGE_PETS_BEFORE_FARMING = {
+    "st_patricks_2025_clover_cow",
+}
+--]]
+getgenv().SETTINGS = {
+    ENABLE_AUTO_FARM = true,
+    SET_FPS = 2,
+    FOCUS_FARM_AGE_POTION = true, 
+    PETS_TO_AGE_IN_PEN = {
+        "cracked_egg",
+        "endangered_2026_endangered_egg",
+        "pet_egg",
+        "royal_egg",
+        "aztec_egg_2025_xiucohtl",
+        "aztec_egg_2025_ehecatl",
+    },
+    
+    ENABLE_RELEASE_PETS = false, -- if true it will use the pets in PETS_TO_AGE_IN_PEN to RELEASE
+    PET_AUTO_FUSION = false,
+    DEBUG_MODE = false,
+    POTATO_MODE = true,
+    HATCH_EGG_PRIORITY = true,
+    HATCH_EGG_PRIORITY_NAMES = {
+        "cracked_egg", 
+        "pet_egg",
+        "royal_egg",
+    },
+    PET_ONLY_PRIORITY = false,
+    PET_ONLY_PRIORITY_NAMES = {
+        "cracked_egg",
+    },
+    ENABLE_TRADE_COLLECTOR = true,
+    TRADE_ONLY_NEON_LUMINOUS_AND_MEGA = true,
+    TRADE_COLLECTOR_NAME = {
+        "input_Username_Here1",
+        "input_Username_Here2_ect",
+    },
+    TRADE_LIST = {
+        "pet_recycler_2025_giant_panda",
+        "ice_dimension_2025_frostbite_bear",
+    },
+    ENABLE_TRASH_COLLECTOR = false, -- it has its own list of trash pets
+    TRASH_COLLECTOR_NAMES = {"input_Username_Here_for_TRASH"},
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7359a8866fd351cc305781fb0b0ecfc.lua"))()
+
+print("✅ Farming script loaded and running!")
 
 
-        ["EggFarmActive"] = false, -- farm egg, if no more eggs will farm potion, if found new egg will farm them to
-        ["EggToIgnore"] = {""}, -- eggs to ignore when eggfarm active
-        ["LoopBuyEgg"] = false, -- when enough bucks will buy egg, only work with eggfarm
-        ["EggToBuyEgg"] = "",
-
-
-        ["NeonMegaFarm"] = false,
-        ["BuyEgg"] = "", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
-        
-
-        ["AutoReleasePet"] = false, -- work with egg/pet farm, will release selected rarity
-        ["RecycleWebhook"] = "",
-        ["common"] = false,
-        ["uncommon"] = false,
-        ["rare"] = false,
-        ["ultra_rare"] = false,
-
-        ["HideUselessGui"] = true,
-        ["Blur_username"] = true,
-        ["Blazing_Lion_Log"] = false,
-        ["DiscordId"] = "123456",
-        ["Webhook"] = "",
-        ["LegendaryWebhook"] = "",
-        ["NeonMegaWebhook"] = "",
-    }
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
-end)
+print("✅ Pet Wear Auto Buyer started in background!")
